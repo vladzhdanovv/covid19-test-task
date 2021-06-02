126cf240f42a0852a575b17e5885aec3553d942c
vladzhdanovv <vladzhdanovv@gmail.com>
2021-06-02 18:45:25 +0300

    Task 7 - Extend Left Sidebar
    Added ability to filter countries in the sidebar

--- src/components/Layout/Sidebar.jsx
+++ src/components/Layout/Sidebar.jsx
@@ -1,22 +1,46 @@
 import React, {useCallback, useEffect, useState} from 'react';
 import {Link} from "react-router-dom";
-import {Nav} from "react-bootstrap";
-import {getCountries} from '../../api/countries'
+import {Form, FormControl, InputGroup, Nav} from "react-bootstrap";
+import {getCountries} from '../../api/countries';
+
 const Sidebar = () => {
+    const [all, setAll] = useState([]);
     const [countries, setCountries] = useState([]);
+    const [query, setQuery] = useState('');
 
     useEffect(async () => {
         const countries = await getCountries()
-        setCountries(countries);
+        setAll(countries);
     }, []);
+
+    useEffect(async () => {
+        const filtered = all.filter(item => item.Country.startsWith(query));
+        setCountries(filtered);
+    }, [all, query])
 
     return (
         <aside style={{minHeight: `calc(100vh - 50px)`, borderRight: '1px solid black', padding: '15px'}}>
             <Nav className="flex-column">
+                <Form>
+                    <InputGroup>
+                        <FormControl
+                            type="text"
+                            placeholder="Enter name"
+                            value={query}
+                            onChange={(e) => setQuery(e.target.value)}
+                        />
+                        <InputGroup.Append style={{cursor: 'pointer'}} onClick={() => setQuery('')}>
+                            <InputGroup.Text>
+                                <span>⎌</span>
+                            </InputGroup.Text>
+                        </InputGroup.Append>
+                    </InputGroup>
+                </Form>
+                <br/>
                 {
-                    countries.map(country => {
+                    countries.map((country, key) => {
                         return (
-                            <Nav.Item key={country.Slug}>
+                            <Nav.Item key={key}>
                                 <Link to={`${country.Slug}`}>
                                     { country.Country}
                                 </Link>
