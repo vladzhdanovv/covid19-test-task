5947af5edcf0d0c513776df617cfe75af91a75ed
vladzhdanovv <vladzhdanovv@gmail.com>
2021-06-02 19:36:15 +0300

    Minor fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            Hi, this is a tool will show the recent COVID-19 stats, just select a country from the sidebar
+            Hi, this is a tool that shows the recent COVID-19 stats, just select a country from the sidebar
         </div>
     );
 }
